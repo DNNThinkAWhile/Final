3fe57ea939c51ad73e930ad6261f3d484167ae63
weitang114.cml20 <weitang114@gmail.com>
2015-06-27 22:29:41 +0800

    read rnn.py fixed to fit new input

--- RNN/read_RNN.py
+++ RNN/read_RNN.py
@@ -1,51 +1,57 @@
 import os
 
-def read_RNN(output, n):
+def read_RNN(inpu,output, n):
     org_tmp = []
     write_tmp = []
     score_index = []
     output_tmp = []
+    index_end = []
     print "Preprocessing input datas..."
-    with open ("../gb", 'r') as result:
+    with open (inpu, 'r') as result:
         lines = result.readlines()
-        org_tmp = lines
+        header_value = 1
         for index, line in enumerate(lines):
-            header_value = (index+1)/n
+            if line.isspace():
+                index_end.append(index - header_value)
+                header_value += 1
+                continue
             write_tmp.append(str(header_value)+" "+line.upper())
         result.close()
-    with open ("../rnn_input", 'w') as rnnfile:
+    with open ("rnn_input", 'w') as rnnfile:
         for content in write_tmp:
             rnnfile.write(content)
         rnnfile.close()
-    cmd = "./simple-examples/simple-examples/rnnlm-0.2b/rnnlm -rnnlm simple-examples/simple-examples/rnnlm-0.2b/model100000 -test ../rnn_input -nbest -debug 0 > ../rnn_scores"
+    cmd = "./simple-examples/simple-examples/rnnlm-0.2b/rnnlm -rnnlm simple-examples/simple-examples/rnnlm-0.2b/model100000 -test rnn_input -nbest -debug 0 > rnn_scores"
     print cmd
     os.system(cmd)
     print "Calculating scores ..."
-    with open ("../rnn_scores", 'r') as rnnfile:
+    with open ("rnn_scores", 'r') as rnnfile:
         lines = rnnfile.readlines()
         tmp_index = 0
         tmp_value = -2000
         for index, line in enumerate(lines):
-            if index % n == 0:
+            if index - 1 in index_end:
                 tmp_index = index
                 tmp_value = float(line)
-            else:
-                tmp_value = float(line) if float(line) < tmp_value else tmp_value
-                tmp_index = index if float(line) < tmp_value else tmp_index
-            if index % n == n - 1:
+            elif float(line) > tmp_value:
+                tmp_value = float(line)
+                tmp_index = index
+            if index in index_end:
+                print "index = ",tmp_index
+                print "value = ",tmp_value
                 score_index.append(tmp_index)
         rnnfile.close()
     for index in score_index:
-        output_tmp.append(org_tmp[index])
+        output_tmp.append(write_tmp[index][2:])
     print "Selecting best sentace..."
-    with open ("../"+output, 'w') as outputfile:
+    with open (output, 'w') as outputfile:
         for content in output_tmp:
             outputfile.write(content)
         outputfile.close()
 
 def main():
     n = 50
-    read_RNN("file", 50)
+    read_RNN("gb","file", 50)
 
 if __name__ == "__main__":
     main()
